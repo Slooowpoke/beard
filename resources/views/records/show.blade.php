6c019ddd070d07bafa53f1c16cb1fda0ceb7a152
Mali <malii.coxon@gmail.com>
2017-07-08 17:15:03 +0100

    Enhanced browsing by utilising ajax

--- resources/views/records/show.blade.php
+++ resources/views/records/show.blade.php
@@ -1,47 +1,65 @@
 @extends('layouts.master')
 
 @section('content')
-	<section class="show">
+	<section class="creator show">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
-					<img class="img-responsive" src="{{ $record->data }}" style="background-image:url('imgs/base.png'); background-size:contain;">
-					<button onclick="upvote({{ $record->id }})">Upvote</button>
-					<button onclick="downvote({{ $record->id }})">Downvote</button>
-					<a href="/{{ $record->id }}">View</a>
-					<a href="/{{ $record->id }}/next">next</a>
-					<a href="/{{ $record->id }}/prev">prev</a>
+					<div class="editor face">
+						<img id="beard" class="img-responsive" src="{{ $record->data }}" style="background-image:url('/imgs/base.png'); background-size:contain;">
+						<button id="upvote" class="upvote" onclick="upvote({{ $record->id }})">Upvote</button>
+						<button id="downvote" class="downvote" onclick="downvote({{ $record->id }})">Downvote</button>
+
+						<a class="prev" id="prev" onclick="view({{ ($record->id)-1 }})">PREV</a>
+						<a class="next" id="next" onclick="view({{ ($record->id)+1 }})">NEXT</a>
+
+						{{-- <a class="next" href="/{{ $record->id }}/next">next</a>
+						<a class="prev" href="/{{ $record->id }}/prev">prev</a> --}}
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	<script>
-	function upvote(id){
-		axios.post('/' + id + '/upvote', {
-				upvote: upvote,
-			})
-			.then(function (response) {
-				console.log(response);
-			})
-			.catch(function (error) {
-				console.log(error);
+	var total = {{ $total }};
+
+	function view(id){
+		if(id < 1){
+			id = total;
+		}else if(id > total){
+			id = 1;
+		}
+
+		axios.post('/show', {
+			id: id,
+		})
+		.then(function (response) {
+			console.log(response);
+			window.history.pushState({page: "another"}, "another page", id);
+
+			var next = document.getElementById('next');
+			next.setAttribute('onclick',  'view('+ (id+1) +')');
+
+			var prev = document.getElementById('prev');
+			prev.setAttribute('onclick',  'view('+ (id-1) +')');
+
+			var upvote = document.getElementById('upvote');
+			upvote.setAttribute('onclick',  'upvote('+ (id) +')');
+
+			var downvote = document.getElementById('downvote');
+			downvote.setAttribute('onclick',  'downvote('+ (id) +')');
+
+			var beard = document.getElementById('beard');
+			beard.setAttribute('src',  response.data.data);
+		})
+		.catch(function (error) {
+			console.log(error);
 		});
 	}
-
-	function downvote(id){
-		axios.post('/' + id + '/downvote', {
-				downvote: downvote,
-			})
-			.then(function (response) {
-				console.log(response);
-			})
-			.catch(function (error) {
-				console.log(error);
-		});
-	}
-
 	</script>
+
+
+	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
 @endsection
