1e5027432c30100f972f7b81c903b5a71c79526a
Mali <malii.coxon@gmail.com>
2017-07-08 17:15:55 +0100

    Added new font, moved upvote functions here.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,21 +8,51 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'BEARDDDDDD') }}</title>
+    <title>Beardinator</title>
 
     <!-- Styles -->
-	<link href="https://fonts.googleapis.com/css?family=Rye|Poppins" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
     <body>
-        <div class="container">
-			<div class="row">
-				<div class="col-xs-12">
-					<h1 class="title">The Barbershop.</h1>
+		<header>
+	        <div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="/"><img class="img-responsive" src="/imgs/logo.png"></a>
+					</div>
 				</div>
-			</div>
-        </div>
+	        </div>
+		</header>
 
 		@yield('content')
     </body>
+
+
+	<script>
+	function upvote(id){
+		axios.post('/' + id + '/upvote', {
+				upvote: upvote,
+			})
+			.then(function (response) {
+				location.reload()
+			})
+			.catch(function (error) {
+				console.log(error);
+		});
+	}
+
+	function downvote(id){
+		axios.post('/' + id + '/downvote', {
+				downvote: downvote,
+			})
+			.then(function (response) {
+				location.reload()
+			})
+			.catch(function (error) {
+				console.log(error);
+		});
+	}
+
+	</script>
 </html>
